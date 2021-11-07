1a583ecc4bb25de1a426371ddc41670efabcc009
Sharad Raj <dev_sharad@outlook.com>
2021-11-07 08:18:16 +0000

    Indent fix

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -54,7 +54,7 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-      sort: { fields: frontmatter___date, order: DESC}
+      sort: { fields: frontmatter___date, order: DESC }
       limit: 1000
       filter: { fields: { tags: { in: [$tag] } } }
     ) {
